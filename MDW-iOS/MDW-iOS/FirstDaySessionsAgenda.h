5ee66d08099b7250828a04d350a793cf33defa3a
JETS <jets@learniosfreecoms-mac.local>
2017-04-14 17:37:50 +0100

    Agenda Ui with pellTo refresh and Html Label now done waiting for real data

---
+++ MDW-iOS/MDW-iOS/FirstDaySessionsAgenda.h
@@ -0,0 +1,13 @@
+//
+//  FirstDaySessionsAgenda.h
+//  MDW-iOS
+//
+//  Created by JETS on 4/14/17.
+//  Copyright © 2017 MAD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstDaySessionsAgenda : UITableViewController
+
+@end
